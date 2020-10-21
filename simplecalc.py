d3f2d9620681ab75809aa9d0ece13ead54dc4806
romanrichter1991 <romanlukava1991@gmail.com>
2020-10-21 11:12:53 +0300

    first commit

---
+++ simplecalc.py
@@ -0,0 +1,13 @@
+
+UserName = input("Please, enter your name: ")
+
+
+A = input("Enter variable A: ")
+B = input("Enter variable B: ")
+C = input("Enter variable C: ")
+
+MathCalc = ((int(A) + int(B)) * int(C))
+
+input("Hello " + UserName.upper() + "!" + "\nPress Enter to see calculation results!")
+
+print("\nCalcultion Result for:",str(A),"+",str(B),"*",str(C),"=",str(MathCalc))
